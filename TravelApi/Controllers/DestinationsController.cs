54cd4604b81a4a541ad7fcaf246f28bb90dd0e2d
Jieun Kang <jieunkang101@gmail.com>
2020-03-30 11:03:28 -0700

    add Destinations Controller with basic CRUD method

---
+++ TravelApi/Controllers/DestinationsController.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelApi.Models;
+
+namespace TravelApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DestinationsController : ControllerBase
+  {
+    private TravelApiContext _db;
+
+    public DestinationsController(TravelApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/destinations
+    [HttpGet]
+    public ActionResult<IEnumerable<Destination>> Get()
+    {
+      return _db.Destinations.ToList();
+    }
+
+    // POST api/destinations
+    [HttpPost]
+    public void Post([FromBody] Destination destination)
+    {
+      _db.Destinations.Add(destination);
+      _db.SaveChanges();
+    }
+
+    // GET api/destinations/3
+    [HttpGet("{id}")]
+    public ActionResult<Destination> Get(int id)
+    {
+      return _db.Destinations.FirstOrDefault(entry => entry.DestinationId == id);
+    }
+
+    // PUT api/destinations/3
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Destination destination)
+    {
+      destination.DestinationId = id;
+      _db.Entry(destination).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/destinations/3
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var destinationToDelete = _db.Destinations.FirstOrDefault(entry => entry.DestinationId == id);
+      _db.Destinations.Remove(destinationToDelete);
+      _db.SaveChanges();
+    }
+
+
+  }
+}    
