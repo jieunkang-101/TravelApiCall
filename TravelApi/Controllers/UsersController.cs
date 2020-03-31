82d18d4787cd032a18640e547c10f0e9452f4ee9
Jieun Kang <jieunkang101@gmail.com>
2020-03-31 13:41:15 -0700

    add Modles/Role.cs and update UsersController.cs for Role Based Authorization

--- TravelApi/Controllers/UsersController.cs
+++ TravelApi/Controllers/UsersController.cs
@@ -6,37 +6,57 @@ using TravelApi.Models;
 namespace TravelApi.Controllers
 {
   [Authorize]
-    [ApiController]
-    [Route("[controller]")]
-    public class UsersController : ControllerBase
+  [ApiController]
+  [Route("[controller]")]
+  public class UsersController : ControllerBase
+  {
+    private IUserService _userService;
+
+    public UsersController(IUserService userService)
     {
-      private IUserService _userService;
-
-      public UsersController(IUserService userService)
-      {
-        _userService = userService;
-      }
-
-      // POST users/authenticate
-      [AllowAnonymous]
-      [HttpPost("authenticate")]
-      public IActionResult Authenticate([FromBody]User userParam)
-      {
-        var user = _userService.Authenticate(userParam.Username, userParam.Password);
-
-        if (user == null)
-            return BadRequest(new { message = "Username or password is incorrect" });
-
-        return Ok(user);
-      }
-
-
-      // GET users
-      [HttpGet]
-      public IActionResult GetAll()
-      {
-        var users =  _userService.GetAll();
-        return Ok(users);
-      }
+      _userService = userService;
     }
+
+    // POST users/authenticate
+    [AllowAnonymous]
+    [HttpPost("authenticate")]
+    public IActionResult Authenticate([FromBody]User userParam)
+    {
+      var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+      if (user == null)
+          return BadRequest(new { message = "Username or password is incorrect" });
+
+      return Ok(user);
+    }
+
+
+    // GET users
+    [Authorize(Roles = Role.Admin)]
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+      var users =  _userService.GetAll();
+      return Ok(users);
+    }
+
+    // GET users/2
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+      var user =  _userService.GetById(id);
+
+      if (user == null) {
+          return NotFound();
+      }
+
+      // only allow admins to access other user records
+      var currentUserId = int.Parse(User.Identity.Name);
+      if (id != currentUserId && !User.IsInRole(Role.Admin)) {
+          return Forbid();
+      }
+
+      return Ok(user);
+    }
+  }
 }
