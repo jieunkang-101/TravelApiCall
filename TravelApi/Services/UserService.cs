1502ee3df068201ef3fbacdf9ec0388e55e06ce3
Jieun Kang <jieunkang101@gmail.com>
2020-03-31 13:46:57 -0700

    update UserService.cs with GetById() method

--- TravelApi/Services/UserService.cs
+++ TravelApi/Services/UserService.cs
@@ -15,6 +15,7 @@ namespace TravelApi.Services
   {
     User Authenticate(string username, string password);
     IEnumerable<User> GetAll();
+    User GetById(int id);
   }
 
   public class UserService : IUserService
@@ -22,7 +23,8 @@ namespace TravelApi.Services
     // users hardcoded for simplicity, store in a db with hashed passwords in production applications
     private List<User> _users = new List<User>
     { 
-      new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" } 
+      new User { Id = 1, FirstName = "Admin", LastName = "User", Username = "admin", Password = "admin", Role = Role.Admin },
+      new User { Id = 2, FirstName = "Normal", LastName = "User", Username = "user", Password = "user", Role = Role.User } 
     };
 
     private readonly AppSettings _appSettings;
@@ -41,7 +43,7 @@ namespace TravelApi.Services
       {
         return null;
       }
-      
+
       // authentication successful so generate jwt token
       var tokenHandler = new JwtSecurityTokenHandler();
       var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
@@ -49,7 +51,8 @@ namespace TravelApi.Services
       {
         Subject = new ClaimsIdentity(new Claim[] 
         {
-          new Claim(ClaimTypes.Name, user.Id.ToString())
+          new Claim(ClaimTypes.Name, user.Id.ToString()),
+          new Claim(ClaimTypes.Role, user.Role)
         }),
         Expires = DateTime.UtcNow.AddDays(7),
         SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
@@ -71,6 +74,18 @@ namespace TravelApi.Services
         x.Password = null;
         return x;
       });
+    }
+
+    public User GetById(int id) 
+    {
+      var user = _users.FirstOrDefault(x => x.Id == id);
+
+      // return user without password
+      if (user != null) 
+      {
+        user.Password = null;
+      }
+      return user;
     }
   }
 }
