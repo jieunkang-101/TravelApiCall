4112800b1d15b929bac53808be830b7453638a48
Jieun Kang <jieunkang101@gmail.com>
2020-03-31 14:42:42 -0700

    update UsersController.cs to include registration and standard CRUD operations

--- TravelApi/Controllers/UsersController.cs
+++ TravelApi/Controllers/UsersController.cs
@@ -1,5 +1,15 @@
-using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+using TravelApi.Dtos;
+using TravelApi.Helpers;
 using TravelApi.Services;
 using TravelApi.Models;
 
@@ -11,25 +21,72 @@ namespace TravelApi.Controllers
   public class UsersController : ControllerBase
   {
     private IUserService _userService;
+    private IMapper _mapper;
+    private readonly AppSettings _appSettings;
 
-    public UsersController(IUserService userService)
+    public UsersController(IUserService userService, IMapper mapper, IOptions<AppSettings> appSettings)
     {
       _userService = userService;
+      _mapper = mapper;
+      _appSettings = appSettings.Value;
     }
 
     // POST users/authenticate
     [AllowAnonymous]
     [HttpPost("authenticate")]
-    public IActionResult Authenticate([FromBody]User userParam)
+    public IActionResult Authenticate([FromBody]UserDto userDto)
     {
-      var user = _userService.Authenticate(userParam.Username, userParam.Password);
+      var user = _userService.Authenticate(userDto.Username, userDto.Password);
 
       if (user == null)
-          return BadRequest(new { message = "Username or password is incorrect" });
+      {
+        return BadRequest(new { message = "Username or password is incorrect" });
+      }
+         
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(new Claim[] 
+        {
+          new Claim(ClaimTypes.Name, user.Id.ToString())
+        }),
+        Expires = DateTime.UtcNow.AddDays(7),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      var tokenString = tokenHandler.WriteToken(token);
 
-      return Ok(user);
+      // return basic user info (without password) and token to store client side
+      return Ok(new {
+        Id = user.Id,
+        Username = user.Username,
+        FirstName = user.FirstName,
+        LastName = user.LastName,
+        Token = tokenString
+      });   
     }
 
+    // POST users/register
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public IActionResult Register([FromBody]UserDto userDto)
+    {
+      // map dto to entity
+      var user = _mapper.Map<User>(userDto);
+
+      try 
+      {
+        // save 
+        _userService.Create(user, userDto.Password);
+        return Ok();
+      } 
+      catch(AppException ex)
+      {
+        // return error message if there was an exception
+        return BadRequest(new { message = ex.Message });
+      }
+    }
 
     // GET users
     [Authorize(Roles = Role.Admin)]
@@ -37,7 +94,8 @@ namespace TravelApi.Controllers
     public IActionResult GetAll()
     {
       var users =  _userService.GetAll();
-      return Ok(users);
+      var userDtos = _mapper.Map<IList<UserDto>>(users);
+      return Ok(userDtos);
     }
 
     // GET users/2
@@ -45,6 +103,7 @@ namespace TravelApi.Controllers
     public IActionResult GetById(int id)
     {
       var user =  _userService.GetById(id);
+      var userDto = _mapper.Map<UserDto>(user);
 
       if (user == null) {
           return NotFound();
@@ -56,7 +115,36 @@ namespace TravelApi.Controllers
           return Forbid();
       }
 
-      return Ok(user);
+      return Ok(userDto);
+    }
+
+    // PUT users/2
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, [FromBody]UserDto userDto)
+    {
+      // map dto to entity and set id
+      var user = _mapper.Map<User>(userDto);
+      user.Id = id;
+
+      try 
+      {
+        // save 
+        _userService.Update(user, userDto.Password);
+        return Ok();
+      } 
+      catch(AppException ex)
+      {
+        // return error message if there was an exception
+        return BadRequest(new { message = ex.Message });
+      }
+    }
+
+    // DELETE users/2
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+      _userService.Delete(id);
+      return Ok();
     }
   }
 }
